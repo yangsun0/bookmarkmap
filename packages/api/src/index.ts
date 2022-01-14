b82d069e59fa281107146e4afccbd214388538d0
Yang Sun <yangsun@lcodej.com>
2022-01-14 11:55:30 -0800

    add api with typescript

---
+++ packages/api/src/index.ts
@@ -0,0 +1,9 @@
+import express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(4000, () => {
+  console.log(`Example app listening at http://localhost:4000`);
+});
