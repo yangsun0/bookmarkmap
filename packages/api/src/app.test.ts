8de985b16fee75c070d7a020c2a2251a0fac0dc6
Yang Sun <yangsun@lcodej.com>
2022-01-21 16:54:23 -0800

    fix test

--- packages/api/src/app.test.ts
+++ packages/api/src/app.test.ts
@@ -4,5 +4,5 @@ import app from "./app";
 test("test example", async () => {
   const res = await request(app).get("/");
   expect(res.statusCode).toBe(200);
-  expect(res.text).toBe("Hello World!");
+  expect(res.text).toBe("Hello World 2!");
 });
